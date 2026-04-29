d6c9a03aceb81f9e43d9d66548b6b5a70a155648
GadgetHub123 <tjayyy78@gmail.com>
2026-04-29 14:12:15 +0800

    fix nodemailer build error

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  serverExternalPackages: ["nodemailer"],
 };
 
 export default nextConfig;
